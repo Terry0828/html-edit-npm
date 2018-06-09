4f94e1d621764e6f1eb84d2b47484685fd2791a7
wenjiawei <wenjiawei@innobuddy.com>
2018-06-10 00:27:33 +0800

    测试接口

--- server/routers/download.js
+++ server/routers/download.js
@@ -6,15 +6,40 @@ const fs = require('fs')
 const os = require('os')
 
 const config = require('../../config/config')
-const { 
+const {
   _compressedFile,
-  _getAllFiles } = require('../utils/utils')
+  _getAllFiles,
+  _isExists,
+  _createFolder,
+  _createFile } = require('../utils/utils')
 
-apiRoutes.get('/download', (req, res) => {
+
+apiRoutes.post('/add', (req, res) => {
+  const query = req.body
+  console.log('body', req.body)
+  // _isExists(config.file.build + '/css/a.txt')
+  // .then(res => {
+  //   return _createFile(config.file.build + '/css/a.txt', 'this is a file for test222!')
+  // })
+  // .then(res => res ? console.log('the file is create success!') : console.log('the file is exists!'))
+
+  return res.json({
+    code: 0,
+    message: "success"
+  })
+})
+
+apiRoutes.get('/zip', (req, res) => {
   const query = req.query
 
-  const filesArr = _getAllFiles()
-  _compressedFile(filesArr)
+  _isExists()
+  .then(res => {
+    if(res) { return false }
+    else { return _createFolder() }
+  })
+  .then(res => _getAllFiles())
+  .then(res => _compressedFile(res))
+
 
   return res.json({
     code: 0,
