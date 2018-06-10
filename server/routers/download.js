702f701e3580971285d9f3959588109d7298d2de
wenjiawei <wenjiawei@innobuddy.com>
2018-06-10 18:31:38 +0800

    测试接口

--- server/routers/download.js
+++ server/routers/download.js
@@ -11,13 +11,25 @@ const {
   _getAllFiles,
   _isExists,
   _createFolder,
-  _createFile } = require('../utils/utils')
+  _createFile,
+  _deleteFile,
+  _deleteFolder,
+  _getFileType,
+  _readFile } = require('../utils/utils')
 
 
 apiRoutes.post('/add', (req, res) => {
   const query = req.body
-  console.log('body', req.body)
-  // _isExists(config.file.build + '/css/a.txt')
+  // console.log('body', req.body)
+
+  _readFile(config.file.build + '/css/a.txt')
+  .then(res => console.log('res', res))
+  
+  // _getFileType(config.file.build + '/css/a.txt')
+  // .then(res => console.log(res))
+  // .catch(err => console.log('err', err))
+
+  // _isExists(config.file.build + '/css/a.txt').then(res => console.log(res))
   // .then(res => {
   //   return _createFile(config.file.build + '/css/a.txt', 'this is a file for test222!')
   // })
@@ -37,8 +49,11 @@ apiRoutes.get('/zip', (req, res) => {
     if(res) { return false }
     else { return _createFolder() }
   })
-  .then(res => _getAllFiles())
+  .then(() => _getAllFiles())
   .then(res => _compressedFile(res))
+  .then(() => _deleteFolder(config.file.build + '/css'))
+  .then(res => console.log('res', res))
+  .catch(err => console.log('err', err))
 
 
   return res.json({
