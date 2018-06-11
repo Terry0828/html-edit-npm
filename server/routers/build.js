4a895f313934a84a3973a051057173e2b0a2ce35
wenjiawei <wenjiawei@innobuddy.com>
2018-06-11 15:52:01 +0800

    测试接口

--- server/routers/build.js
+++ server/routers/build.js
@@ -1,30 +1,40 @@
 const express = require('express')
 const apiRoutes = express.Router()
 const ejs = require('ejs')
+const htmlMinify = require('html-minifier').minify
 
-const config = require('../../config/config')
+const config = require('../config')
 const {
   _readFile,
   _createFile } = require('../utils/utils')
 
 const data = {
-  title: '测试',
-  list: 'ssss'
+  filename: config.path.module + '/index.html',
+  config: {
+    mobile: 'pc', // app 会渲染 rem 适配代码
+    title: '测试',
+    desc: '1111111',
+    key: ''
+  },
+  data: {
+    list: 'ssafasf',
+    js: '<script>console.log("sda")</script>'
+  }
 }
 
 apiRoutes.get('/build', (req, res) => {
-  _readFile(config.file.module + '/index.html')
-  .then(res => {
-    console.log(res)
-    const re = ejs.render(res, data)
-    return _createFile(config.file.result + '/index.html', re)
+  _readFile(config.path.module + '/index.html')
+  .then(result => {
+    const module = ejs.render(result, data)
+    return _createFile(config.path.result + '/index.html', htmlMinify(module, config.HtmlMinify))
+    // return _createFile(config.file.result + '/index.html', module)
   })
-  .then(() => res.json(
-    {
+  .then(() => {
+    return res.json({
       code: 0,
       message: "success"
-    }
-  ))
+    })
+  })
 })
 
 
