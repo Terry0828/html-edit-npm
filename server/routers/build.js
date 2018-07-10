3432d067b8683439d5b26ba93a6cc6e622b84bc9
wenjiawei <wenjiawei@innobuddy.com>
2018-07-10 15:47:31 +0800

    完善构建代码的接口 /api/build POST

--- server/routers/build.js
+++ server/routers/build.js
@@ -7,7 +7,8 @@ const {
   _readFile,
   _createFile,
   _copyFolder,
-  _copyFile } = require('../utils/file')
+  _copyFile,
+  _deleteFolder } = require('../utils/file')
 
 const {
   _JSCodeMinify,
@@ -17,61 +18,72 @@ const {
   _jsLink,
   _CreateHtmlCode } = require('../utils/code')
 
-apiRoutes.post('/test', (req, res) => {
+apiRoutes.post('/save', (req, res) => {
   const code = JSON.parse(req.body.data)
-  // console.log(code.html)
-  _CreateHtmlCode(code.html.layout, code.html.attr)
-  .then(result => {
-    return res.json({
-      code: 0,
-      message: "success",
-      data: result
-    })
-  })
-  
 })
 
-apiRoutes.post('/build', (req, res) => {
-  const _body = JSON.parse(req.body.html),
-    _data = _body.data,
-    _link= _body.link,
-    _config = _body.config,
-    _js = _data.js,
-    _style = _data.style,
-    _layout = _data.layout
-  
-  console.log(_styleLink(_link.style))
 
+const handleFile = () => {
+  // 处理文件
+  return _copyFile(config.path.module + '/favicon.ico', config.path.build + '/favicon.ico')
+  .then(() => _copyFolder(config.path.module + '/js', config.path.build + '/js'))
+  .then(() => _copyFolder(config.path.module + '/css', config.path.build + '/css'))
+  .then(() => _copyFolder(config.path.module + '/static', config.path.build + '/static'))
+}
+const handleJS = (js) => {
+  // 处理 js 代码
+  return _JSCodeMinify(js)
+  .then(result => {
+    if(result === false) { return false }
+    _createFile(config.path.build + '/js/index.js', result.code)
+  })
+}
+const handleCSS = (style, src) => {
+  // 处理 style 代码
+  return _CSSCodeMinify(style)
+  .then(result => {
+    if(result === false) { return false }
+    src ? _createFile(src, result) : _createFile(config.path.build + '/css/index.css', result)
+  })
+}
+const handleHTML = (body) => {
+  // 处理 html 模版
   const data = {
     filename: config.path.module + '/index.html',
-    ..._config,
-    layout: _layout,
-    linkCSS: _styleLink(_link.style),
-    linkJS: _jsLink(_link.js)
+    ...body.config,
+    linkCSS: _styleLink(body.link.style),
+    linkJS: _jsLink(body.link.js)
   }
-  _JSCodeMinify(_js)
-  .then(res => {
-    if(res === false) { return false }
-    _createFile(config.path.build + '/js/index.js', res.code)
-  })
-  .then(() => _CSSCodeMinify(_style))
-  .then(res => {
-    if(res === false) { return false }
-    _createFile(config.path.build + '/css/index.css', res)
-  })
-  .then(() => _copyFile(config.path.module + '/favicon.ico', config.path.build + '/favicon.ico'))
+  let htmlRes
+
+  return _CreateHtmlCode(body.html.layout, body.html.attr)
+  .then(result => htmlRes = result)
+  // 将结构写入 html
+  .then(() => data.layout = htmlRes.layout)
   .then(() => _readFile(config.path.module + '/index.html'))
   .then(result => {
     const module = ejs.render(result, data)
     return _HTMLMinify(module)
   })
   .then(res => _createFile(config.path.build + '/index.html', res))
+  // 最后将 layout 中的样式单独抽离返回，和下面的样式一起处理
+  .then(() => htmlRes.style)
+}
+
+apiRoutes.post('/build', (req, res) => {
+  const _body = JSON.parse(req.body.data)
+
+  handleFile()
+  .then(() => handleHTML(_body))
+  .then((layoutStr) => handleCSS(layoutStr + _body.style))
+  .then(() => handleJS(_body.js))
   .then(() => {
     return res.json({
       code: 0,
       message: "success"
     })
   })
+
 })
 
 
